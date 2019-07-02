1ef1166195d99b34f59cf7aea814276f684e3ae8
n4226 <mikesamipad1@gmail.com>
2019-07-02 11:05:54 -0400

    working

---
+++ Sources/SwiftRestClient/Encoding/JSONParameterEncoding.swift
@@ -0,0 +1,25 @@
+//
+//  JSONParameterEncoding.swift
+//  SwiftyRestKit
+//
+//  Created by Rodolfo Roca on 12/10/18.
+//  Copyright © 2018 Rodolfo Roca. All rights reserved.
+//
+
+import Foundation
+
+public struct JSONParameterEncoder: ParameterEncoder {
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        
+        do {
+            let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            urlRequest.httpBody = jsonAsData
+            
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        } catch {
+            throw SwiftyRestClientError.encodingFailed
+        }
+    }
+}
---
+++ Sources/SwiftRestClient/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,34 @@
+//
+//  URLParameterEncoder.swift
+//  SwiftyRestKit
+//
+//  Created by Rodolfo Roca on 12/10/18.
+//  Copyright © 2018 Rodolfo Roca. All rights reserved.
+//
+
+import Foundation
+
+public struct URLParameterEncoder: ParameterEncoder {
+    
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        
+        guard let url = urlRequest.url else {
+            throw SwiftyRestClientError.missingURL
+        }
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+            
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/x-www-form-urlencoded; charset=utf8", forHTTPHeaderField: "Content-Type")
+            }
+        }
+    }
+}
